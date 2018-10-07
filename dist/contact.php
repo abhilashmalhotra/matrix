b2ef3a8a1268bb73948cdbd0009d3aaf8b512220
Abhilash <abhilashmalhotra17@gmail.com>
2018-10-07 15:45:44 +0530

    contact done

--- dist/contact.php
+++ dist/contact.php
@@ -33,6 +33,73 @@
 					</div>
 				</div>
 			</section>
+			<section class="map_section">
+				<div class="map-responsive">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14017.174398987561!2d77.1578166!3d28.5609451!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8a5485ace6966f72!2sDentist+in+South+Delhi+-+Matrix+Dental+and+Skin+Lounge!5e0!3m2!1sen!2sin!4v1483509364368" width="600" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+			</section>
+			<section class="matrix-section contact_matrix_dental">
+				<!-- <img src="images/contact-img.jpg" class="img-fluid" alt=""> -->
+				<div class="container">
+					<div class="row">
+						<div class="offset-md-6 col-md-6">
+							<div class="contact_info">
+								<h1 class="title">GET IN TOUCH...</h1>
+								<p class="icon_text address"><img src="images/icons/map.png" class="img-fluid" alt="">Matrix Dental and Skin Lounge, 70, Poorvi Marg, Opposite Modern School, Vasant Vihar , New Delhi - 110057</p>
+								<p class="icon_text phone"><img src="images/icons/phone.png" class="img-fluid" alt=""> +91-8826001000, +91-8826002000</p>
+								<p class="icon_text email"><img src="images/icons/email.png" class="img-fluid" alt=""> <a href="">clinicmatrix@gmail.com</a></p>
+								<form action="" class="form_book">
+									<div class="row">
+										<div class="col-md-4 col-sm-6">
+											<div class="form-group wow fadeInUp">
+												<label for="exampleInputEmail1" class="bmd-label-floating">Name</label>
+												<input type="text" class="form-control" id="exampleInputEmail1">
+												<i class="fas fa-user prefix"></i>
+												<!-- <span class="error text-danger">Please Enter Name</span> -->
+											</div>
+										</div>
+										<div class="col-md-4 col-sm-6">
+											<div class="form-group wow fadeInUp">
+												<label for="exampleInputEmail1" class="bmd-label-floating">Mobile No.</label>
+												<input type="text" class="form-control" id="exampleInputEmail1">
+												<i class="fas fa-mobile-alt prefix"></i>
+												<!-- <span class="error text-danger">Please Enter Mobile No.</span> -->
+											</div>
+										</div>
+										<div class="col-md-4 col-sm-6">
+											<div class="form-group wow fadeInUp">
+												<label for="exampleInputEmail1" class="bmd-label-floating">Email ID</label>
+												<input type="text" class="form-control" id="exampleInputEmail1">
+												<i class="far fa-envelope prefix"></i>
+												<!-- <span class="error text-danger">Please Enter Email ID</span> -->
+											</div>
+										</div>
+										<div class="col-md-12">
+											<div class="form-group wow fadeInUp">
+												<label for="message" class="bmd-label-floating">Message</label>
+												<textarea class="form-control" id="message" rows="3" style="resize: none"></textarea>
+												<i class="fas fa-file-alt prefix"></i>
+											</div>
+											<!-- <div class="form-group wow fadeInUp">
+												<label for="exampleInputEmail1" class="bmd-label-floating">Date</label>
+												<input type="text" class="form-control" id="exampleInputEmail1">
+												<i class="far fa-calendar-alt prefix"></i>
+												<span class="error text-danger">Please Enter Email ID</span>
+											</div> -->
+										</div>
+										<div class="col-md-12 mt-3 wow fadeInUp">
+											<img src="images/captcha.jpg" style="width: 35%" class="img-fluid" alt="">
+										</div>
+										<div class="col-md-12 text-center mt-4 wow fadeInUp">
+											<a href="javascript:void(0)" class="btn btn btn-raised matrix_btn">Send Message</a>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</div>
 	</div>
 	<?php  include 'include/footer.php'; ?>
