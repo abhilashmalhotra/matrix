8ebba0a6e7fa44409f8c42da6b34453e8ab6b705
Abhilash <abhilashmalhotra17@gmail.com>
2018-10-09 01:42:53 +0530

    error page

---
+++ dist/404.php
@@ -0,0 +1,38 @@
+<?php include 'include/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Matrix Delhi Dental</title>	
+	<?php include 'include/head.php'; ?>	
+</head>
+<body>
+	<div class="matrix_container">
+		<!-- Header Include Here -->
+		
+		<?php include 'include/header.php'; ?>
+		<!-- Main Container of Anurag Singh -->
+		<div class="main_matrix_container">
+			
+			<section class="page-error">
+				<div class="container">
+					<div class="row">
+						<div class="col-12">
+							<div class="error-content text-center">
+								<h2>404</h2>
+								<img src="<?php echo $siteUrl ?>images/404-error.png" class="img-fluid" alt="image">
+								<h4>page not found</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+	<?php  include 'include/footer.php'; ?>
+	<?php  include 'include/foot.php'; ?>
+	<?php include 'include/modal.php'; ?>
+	
+
+</body>
+</html>
